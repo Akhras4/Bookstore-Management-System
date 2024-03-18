eeb6e3ee912a1af8b3bc2b130aa4b892fa8d237f
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-03-18 17:30:01 +0100

    JWTAuth

--- controller/functions.js
+++ controller/functions.js
@@ -4,7 +4,9 @@ const crypto=require("crypto")
 const bcrypt=require("bcrypt");
 require('dotenv').config();
 const nodemailer = require("nodemailer");
+const { brotliCompressSync } = require('zlib');
 const PORT = process.env.PORT ;
+const jwt = require('jsonwebtoken');
 
 
 const singup = (req, res) => {
@@ -24,7 +26,7 @@ const singup = (req, res) => {
                    .then(() => {
                     const hash = bcrypt.hashSync(Password , 15)
                     NewUser.Password=hash
-                    NewUser.updateOne(Password)
+                    NewUser.save({ new:true })
                            .then(()=>{sendemailtoclient(email,UserName,emailtoken,PORT) })
                            .then(()=> {res.status(200).json({ _id: NewUser._id, UserName, email, emailtoken })})
                            .catch((error)=>{res.status(400).json(error="faild hash")})
@@ -50,11 +52,23 @@ const tokenval = (req, res) => {
     const emailtoken = req.query.emailtoken;
     if (!emailtoken) return res.status(404).json("Token not found");
     users.findOneAndUpdate({ emailtoken }, { isValid: true, emailtoken: null },{ new:true }) 
-         .then(newUser => {
-            res.status(200).json({ UserName: newUser.UserName, emailtoken: newUser.emailtoken, isValidate: newUser.isValid })})
-         .catch(error => {
+        .then(newUser => {
+            const payload = {
+                userId: newUser._id,
+                UserName: newUser.UserName
+            };
+            jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '4h' }, (err, token) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error: "Failed to generate token" });
+                } else {
+                    res.status(200).json({ token });
+                }
+            })
+        })
+        .catch(error => {
            return res.status(404).json("deed token");
-        });
+        })
 };
 
 
@@ -84,7 +98,6 @@ async function sendemailtoclient(email,UserName,emailtoken,PORT){
     }
 }
 
-
  function HoldingUntileRedirect(email){
                 setTimeout(() => {
                 users.findOne({email}) 
@@ -109,21 +122,52 @@ const login = (req,res)=>{
        users.findOne({email})
             .then((discover)=>{
                console.log(discover)
-               if( Password == discover.Password){
-                   res.status(200).redirect(302,`/${discover.UserName}`,{discover})//302 for now Moved Temporarily
+               bcrypt.compare(Password, discover.Password, (err, result) => {
+                if(err){
+                    res.status(400).json({message:"err"})//.render("login")
+                }
+                if (result){
+                    const payload = {
+                    userId: discover._id,
+                    email: discover.email,
+                    UserName: discover.UserName
+                };
+                console.log(discover)
+                jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '4h' }, (err, token) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).json({ error: "Failed to generate token" });
+                    } else {
+                        res.status(200).json({ token });
+                    }
+                });
                }else{
-                   res.status(400).json({message:"Username or password uncorrect"})//.render("login")
+                return res.status(400).json({success: false, message: 'passwords do not match'})
                }})
+            })
             .catch(error=>{
                 console.error(error); 
-                 res.status(500).json({error:"Internal Server Error"});
+                 res.status(500).json({message:"Internal Server Error"});
             })
     }
+};
+const cookieJWTAuth=(req,res,next)=>{
+    const token =req.cookies.token
+    .then(()=>{
+        const user=jwt.verify(token,process.env.MY_SECRET);
+        req.user=user
+        next()
+    })
+    .catch((err)=>{
+        res.clearCookie("token")
+        return res.redirect("/")
+    })
 }
 
 module.exports = {
     singup,
     tokenval,
     login,
+    cookieJWTAuth,
      
 }
